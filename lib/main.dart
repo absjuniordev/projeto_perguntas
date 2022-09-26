af154c610a8b4cc7a385fe9aa7750fc9ed812196
absjuniordev <abs.junnior@hotmail.com>
2022-09-26 20:25:00 -0300

    Transformacao de Stateless para Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(PerguntaApp());
 
-@override
-class PerguntaApp extends StatelessWidget {
-  final perguntas = ["Qual a sua cor favorita?", "Qual o seu animal favorito?"];
+class PerguntaAppState extends State<PerguntaApp> {
+  //esta classe vai gerenciar o estado
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+  }
 
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual a sua cor favorita?",
+      "Qual o seu animal favorito?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(centerTitle: true, title: const Text('Perguntas')),
         body: Column(
           children: [
-            Text(perguntas[0]),
-            const ElevatedButton(onPressed: null, child: Text("Resposta 1")),
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(onPressed: responder, child: Text("Resposta 1")),
+            ElevatedButton(onPressed: responder, child: Text("Resposta 2")),
+            ElevatedButton(onPressed: responder, child: Text("Resposta 3"))
           ],
         ),
       ),
     );
+  }
+}
+
+@override
+class PerguntaApp extends StatefulWidget {
+  //o 'runApp(PerguntaApp())' identifica q o componente retorna um 'State'
+  PerguntaAppState createState() {
+    //
+    return PerguntaAppState();
   }
 }
