cda62fc67f0170ba63b39d82f256f86975b6cfc9
absjuniordev <abs.junnior@hotmail.com>
2022-09-26 21:36:31 -0300

    Widget Respostas e Callback

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
 void main() => runApp(PerguntaApp());
 
@@ -25,9 +26,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(onPressed: _responder, child: Text("Resposta 1")),
-            ElevatedButton(onPressed: _responder, child: Text("Resposta 2")),
-            ElevatedButton(onPressed: _responder, child: Text("Resposta 3"))
+            Resposta("Resposta 1", _responder),
+            Resposta("Resposta 2", _responder),
+            Resposta("Resposta 3", _responder),
           ],
         ),
       ),
